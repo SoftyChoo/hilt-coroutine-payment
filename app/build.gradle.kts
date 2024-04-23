4eb95a7fd68eed88098e6957a7c8c76bd7639bf1
추민수 <devsoftychoo@gmail.com>
2024-04-23 15:12:39 +0900

    [Pack]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,11 +2,13 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("com.google.gms.google-services")
+    kotlin("kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
     namespace = "com.example.hilt_coroutine_payment"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.hilt_coroutine_payment"
@@ -54,6 +56,10 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
+    // Hilt
+    implementation("com.google.dagger:hilt-android:2.44")
+    kapt("com.google.dagger:hilt-android-compiler:2.44")
+
     // Gson
     implementation("com.google.code.gson:gson:2.10.1")
 
@@ -64,37 +70,39 @@ dependencies {
     // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0")
-
     // Lifecycle
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.5.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation("androidx.activity:activity-ktx:1.7.2")
+    implementation("androidx.fragment:fragment-ktx:1.6.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
 
-    // Navigation
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
+//    // Navigation
+//    implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
+//    implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
 
-    // 카카오 로그인 API 모듈
-    implementation("com.kakao.sdk:v2-common:2.20.1")
-    implementation("com.kakao.sdk:v2-auth:2.20.1")
-    implementation("com.kakao.sdk:v2-user:2.20.1")
-
-
-    // 네이버 로그인 API 모듈
-//    implementation("com.navercorp.nid:oauth:5.9.0") // jdk 11
-    implementation("com.navercorp.nid:oauth-jdk8:5.9.0") // jdk 8
-
-
-    // 구글 로그인 API 모듈
-    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
-    implementation("com.firebaseui:firebase-ui-auth:7.2.0")
-    implementation("com.google.firebase:firebase-auth-ktx")
-    implementation("com.google.android.gms:play-services-auth:20.7.0")
-
-    // 토스 결제 API 모듈
-    implementation("com.github.tosspayments:payment-sdk-android:0.1.14")
+//    // 카카오 로그인 API 모듈
+//    implementation("com.kakao.sdk:v2-common:2.20.1")
+//    implementation("com.kakao.sdk:v2-auth:2.20.1")
+//    implementation("com.kakao.sdk:v2-user:2.20.1")
+//
+//
+//    // 네이버 로그인 API 모듈
+////    implementation("com.navercorp.nid:oauth:5.9.0") // jdk 11
+//    implementation("com.navercorp.nid:oauth-jdk8:5.9.0") // jdk 8
+//
+//
+//    // 구글 로그인 API 모듈
+//    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
+//    implementation("com.firebaseui:firebase-ui-auth:7.2.0")
+//    implementation("com.google.firebase:firebase-auth-ktx")
+//    implementation("com.google.android.gms:play-services-auth:20.7.0")
+//
+//    // 토스 결제 API 모듈
+//    implementation("com.github.tosspayments:payment-sdk-android:0.1.14")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
+
