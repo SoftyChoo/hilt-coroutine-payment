3357f79b039e83e24dd89123af59280e84ae7db4
추민수 <devsoftychoo@gmail.com>
2024-04-24 10:14:09 +0900

    [Feat] : Kakao API 모듈, 권한 설정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,11 +83,14 @@ dependencies {
 //    implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
 //    implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
 
-//    // 카카오 로그인 API 모듈
-//    implementation("com.kakao.sdk:v2-common:2.20.1")
-//    implementation("com.kakao.sdk:v2-auth:2.20.1")
+    // 카카오 로그인 API 모듈
+    implementation ("com.kakao.sdk:v2-all:2.20.1") // 전체 모듈 설치, 2.11.0 버전부터 지원
+    implementation ("com.kakao.sdk:v2-user:2.20.1") // 카카오 로그인 API 모듈
+    implementation ("com.kakao.sdk:v2-share:2.20.1") // 카카오톡 공유 API 모듈
+    implementation("com.kakao.sdk:v2-common:2.20.1")
+    implementation("com.kakao.sdk:v2-auth:2.20.1")
 //    implementation("com.kakao.sdk:v2-user:2.20.1")
-//
+
 //
 //    // 네이버 로그인 API 모듈
 ////    implementation("com.navercorp.nid:oauth:5.9.0") // jdk 11
--- app/src/main/java/com/example/hilt_coroutine_payment/MyApplication.kt
+++ app/src/main/java/com/example/hilt_coroutine_payment/MyApplication.kt
@@ -1,19 +1,25 @@
 package com.example.hilt_coroutine_payment
 
 import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
+
 
 @HiltAndroidApp
 class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-//        KakaoSdk.init(this, getString(R.string.kakao_native_key))
+
+//        val KAKAO_NATIVE_APP_KEY = "cfd52d78a38a0d71c022e4d129d4636e"
+        KakaoSdk.init(this, R.string.KAKAO_NATIVE_APP_KEY.toString())
+
+//        KakaoSdk.init(this, KAKAO_NATIVE_APP_KEY)
 
 //        val OAUTH_CLIENT_ID = "8cWEYtwRpoPkyV4eCLG5"
 //        val OAUTH_CLIENT_SECRET = "ZN2UoEEa4J"
 //        val OAUTH_CLIENT_NAME = BuildConfig.APPLICATION_ID
 //
-//        val KAKAO_NATIVE_APP_KEY = "5f623e886b3a8129a377a1a63c63b015"
+//
 //
 //        // Naver SDK 초기화
 //        NaverIdLoginSDK.initialize(this, OAUTH_CLIENT_ID, OAUTH_CLIENT_SECRET, OAUTH_CLIENT_NAME)
